6d2c2d6926058e47a53d0b70b0d69622e6646797
Greg <billingsgregory2@gmail.com>
2021-01-09 22:06:59 -0800

    Demo code displaying basic structure, console output, console input, start of variables.

---
+++ HelloWorldTutoring/HelloWorldTutoring/HelloWorld.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace HelloWorldTutoring
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            int numberOfYears = 0;
+            Console.WriteLine("Enter your name: ");
+            String inputLine = Console.ReadLine();
+            Console.WriteLine("Hello " + inputLine);
+            Console.Write("Welcome ");
+            Console.Write("to ");
+            Console.Write("programming! ");
+            Console.WriteLine("You have been writing code for " + numberOfYears +  " years");
+            Thread.Sleep(5000);
+        }
+    }
+}
